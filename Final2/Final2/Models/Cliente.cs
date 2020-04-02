33543a1fd2f22eb31458bc2fd5b5a44855ce287f
polanco15 <araidepolanco@gmail,com>
2020-04-02 17:36:52 -0400

    Creacion de controladores
    Aqui se crean los controladores para cada crud y conexion con  base de datos

---
+++ Final2/Final2/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Final2.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public long Cedula { get; set; }
+        public string Nombre { get; set; }
+        public long Telefono { get; set; }
+        public string Email { get; set; }
+        public string Categoria { get; set; }
+    }
+}
---
+++ Final2/Final2/Models/Proveedor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Final2.Models
+{
+    public class Proveedor
+    {
+        public int Id { get; set; }
+        public long Cedula { get; set; }
+        public string Nombre { get; set; }
+        public long Telefono { get; set; }
+        public string Email { get; set; }
+    }
+}
